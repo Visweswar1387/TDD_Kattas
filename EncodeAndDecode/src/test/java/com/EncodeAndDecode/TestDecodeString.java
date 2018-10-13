2af9e4fcc1c1872ed242b4403094723de6f69b83
Visweswar1387 <gvisweswar0@gmail.com>
2018-10-13 22:36:52 +0530

    Add testcase for character frequency to string conversion

--- EncodeAndDecode/src/test/java/com/EncodeAndDecode/TestDecodeString.java
+++ EncodeAndDecode/src/test/java/com/EncodeAndDecode/TestDecodeString.java
@@ -22,4 +22,10 @@ public class TestDecodeString {
     public void shouldTestDecodeStringLengthLessThanZero() {
         Assert.assertEquals("Input string length must should be greater than 0", decodeString.decode(""));
     }
+
+    @Test
+    public void shouldTestConversionFromCharacterFrequencyToString() {
+        ConversionCharacterFrequencyToString conversionCharacterFrequencyToString = new ConversionCharacterFrequencyToString;
+        Assert.assertEquals("aa", conversionCharacterFrequencyToString.characterFrequencyToString("a2"));
+    }
 }
