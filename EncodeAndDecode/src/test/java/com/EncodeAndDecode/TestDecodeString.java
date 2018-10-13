3eb8b2fff33056e17ce289225aed7a0d0a4217af
Visweswar1387 <gvisweswar0@gmail.com>
2018-10-13 16:24:56 +0530

    Refactor code for DecodeString

--- EncodeAndDecode/src/test/java/com/EncodeAndDecode/TestDecodeString.java
+++ EncodeAndDecode/src/test/java/com/EncodeAndDecode/TestDecodeString.java
@@ -1,14 +1,20 @@
 package com.EncodeAndDecode;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestDecodeString {
 
+    private DecodeString decodeString;
+
+    @Before
+    public void initialise() {
+        decodeString= new DecodeString();
+    }
+
     @Test
     public void shouldTestDecodeStringLengthGreaterThanZero() {
-
-        DecodeString decodeString = new DecodeString();
         Assert.assertEquals("2", decodeString.decode("a1"));
     }
 }
