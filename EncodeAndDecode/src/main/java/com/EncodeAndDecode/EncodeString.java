34bc9159ddbb4053ebcc16ee41639861c8182565
Visweswar1387 <gvisweswar0@gmail.com>
2018-10-13 10:19:05 +0530

    Add code for find string length.

--- EncodeAndDecode/src/main/java/com/EncodeAndDecode/EncodeString.java
+++ EncodeAndDecode/src/main/java/com/EncodeAndDecode/EncodeString.java
@@ -2,6 +2,9 @@ package com.EncodeAndDecode;
 
 public class EncodeString {
     public String encode(String inputString) {
+        if(inputString.length()>0) {
+            return "Length Greater than zero";
+        }
         return "";
     }
 }
