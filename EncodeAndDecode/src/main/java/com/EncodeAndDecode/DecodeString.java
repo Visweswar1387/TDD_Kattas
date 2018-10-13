77eef1206f127e38e772a2b74ce82812ff0b2df1
Visweswar1387 <gvisweswar0@gmail.com>
2018-10-13 23:01:04 +0530

    Remove magic numbers in decode string

--- EncodeAndDecode/src/main/java/com/EncodeAndDecode/DecodeString.java
+++ EncodeAndDecode/src/main/java/com/EncodeAndDecode/DecodeString.java
@@ -1,8 +1,9 @@
 package com.EncodeAndDecode;
 
 public class DecodeString {
+    private final int ZERO=0;
     public String decode(String inputString) {
-        if(inputString.length()>0) {
+        if(inputString.length()>ZERO) {
             return ""+inputString.length();
         }
         return "Input string length must should be greater than 0";
