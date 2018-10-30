8456d0739e405bd79b8c34670ffdce25aa850fc3
Visweswar1387 <gvisweswar0@gmail.com>
2018-10-31 00:02:06 +0530

    Add testcases for connection pooling

--- Assignment_3_1/src/test/java/TestDbConnection.java
+++ Assignment_3_1/src/test/java/TestDbConnection.java
@@ -1,15 +1,41 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.sql.Connection;
+
 public class TestDbConnection {
-    @Test
-    public void shouldTestDbConnection() {
-        DbConnection.getConnection();
-        DbConnection.getConnection();
-        DbConnection.getConnection();
-        DbConnection.getConnection();
-        DbConnection.getConnection();
-        if(DbConnection.getConnection()==null) {
-            System.out.print("Connection Pool is limited.");
-        }
+
+
+
+    private static Connection connection1;
+    private static Connection connection2;
+    private static Connection connection3;
+    private static Connection connection4;
+    private static Connection connection5;
+
+    @BeforeClass
+    public static void intialise() throws PoolLimitException {
+        ConnectionPool.intialisePool();
+        connection1 = ConnectionPool.getConnection();
+        connection2 = ConnectionPool.getConnection();
+        connection3 = ConnectionPool.getConnection();
+        connection4 = ConnectionPool.getConnection();
+        connection5 = ConnectionPool.getConnection();
+
     }
+
+    @Test(expected = PoolLimitException.class)
+    public void shouldTestDbConnectionLimitException() throws PoolLimitException {
+        Assert.assertEquals("Sorry Pool Limit is Reached.",ConnectionPool.getConnection());
+    }
+
+    @Test
+    public void shouldTestDbConnectionReuseConnection() throws PoolLimitException {
+        ConnectionPool.closeConnection(connection1);
+        Assert.assertEquals(connection1,ConnectionPool.getConnection());
+    }
+
+
 }
