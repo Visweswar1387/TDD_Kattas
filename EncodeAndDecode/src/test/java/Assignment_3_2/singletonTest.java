667e221702ffd67e5508595d20c1652e24b99575
Visweswar1387 <gvisweswar0@gmail.com>
2018-11-03 09:45:31 +0530

    Add test for chaecking hascode of singleton objects:

--- EncodeAndDecode/src/test/java/Assignment_3_2/singletonTest.java
+++ EncodeAndDecode/src/test/java/Assignment_3_2/singletonTest.java
@@ -4,12 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class singletonTest {
-
     @Test
     public void shouldTestSingleton() {
 
         Singleton instance1 = Singleton.INSTANCE;
         Singleton instance2 = Singleton.INSTANCE;
         Assert.assertEquals(instance1, instance2);
+    }
+    @Test
+    public void shouldTestSameReference() {
+        Singleton instance1 = Singleton.INSTANCE;
+        Singleton instance2 = Singleton.INSTANCE;
+        Assert.assertEquals(instance1.hashCode(),instance2.hashCode());
     }
 }
